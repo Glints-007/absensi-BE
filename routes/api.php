a8678f5badb531bcb4e314b5071af6e056a64d2d
andranugraha <73644729+andranugraha@users.noreply.github.com>
2021-09-16 18:26:47 +0700

    Create api route for login and logout

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('v1/login', [App\Http\Controllers\AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('v1/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+});
